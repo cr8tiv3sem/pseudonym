e9be9474ef823a84e4d128bfc2f6b6130ddf30a9
Matthew miller <m177illerdaggeroftruth@gmail.com>
2016-01-30 16:04:34 -0500

    Create Script.php
    Php test get  php echo

---
+++ Img1src.index/Script.php
@@ -0,0 +1,22 @@
+<?php 
+
+  
+Get_elements(), {
+    
+   If (have IMG_src()) : 
+  While (have IMG_SRC());
+ 
+?>
+<?
+    <element class="element";> 
+  <p><a img src="<?"img src()";></p><a img src=(<?"the_IMG()";?><?"img_id"?>)</a></p> 
+     </element class> ?>
+
+<?php end while:
+
+    Else:
+    Echo '<p>no elements found</p>'
+
+  End if;
+
+?>
